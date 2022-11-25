9d22d194c3a3ca849ba6052970f39cbfba3e3fc7
GenkiHirano <genki86web@gmail.com>
2022-11-25 23:58:19 +0900

    feat: ログメッセージ追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,6 +23,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("start server")
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
